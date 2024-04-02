bf90ce6c310a68d04f208bfb4d742fbe3bfabc93
Jisuk <sue030124@naver.com>
2024-04-02 17:51:43 +0900

    code update

--- gaze_tracking/gaze_tracking.py
+++ gaze_tracking/gaze_tracking.py
@@ -25,7 +25,7 @@ class GazeTracking(object):
 
         # _predictor is used to get facial landmarks of a given face
         cwd = os.path.abspath(os.path.dirname(__file__))
-        model_path = os.path.abspath(os.path.join(cwd, "trained_models/mmod_human_face_detector.dat"))
+        model_path = os.path.abspath(os.path.join(cwd, "trained_models/shape_predictor_68_face_landmarks.dat"))
         self._predictor = dlib.shape_predictor(model_path)
 
     @property
--- tracking_test.py
+++ tracking_test.py
@@ -1,8 +1,6 @@
 from __future__ import division
-import cv2
+import cv2 as cv
 from gaze_tracking.gaze_tracking import GazeTracking
-from gaze_tracking.eye import Eye
-from gaze_tracking.calibration import Calibration
 
 
 # mmod_human_face_detector.dat 파일의 경로 설정
@@ -10,7 +8,7 @@ detector_path = "gaze_tracking/trained_models/mmod_human_face_detector.dat"
 
 # GazeTracking 객체를 생성
 gaze = GazeTracking(detector_path)
-webcam = cv2.VideoCapture(0)
+webcam = cv.VideoCapture(0, cv.CAP_DSHOW)
 
 while True:
     # We get a new frame from the webcam
@@ -31,17 +29,17 @@ while True:
     elif gaze.is_center():
         text = "Looking center"
 
-    cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
+    cv.putText(frame, text, (90, 60), cv.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
 
     left_pupil = gaze.pupil_left_coords()
     right_pupil = gaze.pupil_right_coords()
-    cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
-    cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+    cv.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+    cv.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
 
-    cv2.imshow("Demo", frame)
+    cv.imshow("Tracking Test", frame)
 
-    if cv2.waitKey(1) == 27:
+    if cv.waitKey(1) == 27:
         break
 
 webcam.release()
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
