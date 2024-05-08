678d43367a91885e578baebbea07571756f636ce
unoe <157384786+unoeonu@users.noreply.github.com>
2024-05-08 13:35:32 +0900

    hog_altino

---
+++ editing.py
@@ -0,0 +1,93 @@
+"""
+Demonstration of the GazeTracking library.
+Check the README.md for complete documentation.
+"""
+import time
+import cv2
+from gaze_tracking.gaze_tracking import GazeTracking
+from Altino import *
+
+
+
+gaze = GazeTracking()
+webcam = cv2.VideoCapture(0)
+
+
+def cam() :
+
+    global left_pupil
+    global right_pupil
+
+    while True:
+        # We get a new frame from the webcam
+        _, frame = webcam.read()
+
+        # We send this frame to GazeTracking to analyze it
+        gaze.refresh(frame)
+
+        frame = gaze.annotated_frame()
+        text = ""
+
+        if gaze.is_blinking():
+            text = "Blinking"
+        elif gaze.is_right():
+            text = "Looking right"
+        elif gaze.is_left():
+            text = "Looking left"
+        elif gaze.is_center():
+            text = "Looking center"
+
+        cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
+
+        left_pupil = gaze.pupil_left_coords()
+        right_pupil = gaze.pupil_right_coords()
+        cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+        cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+
+        cv2.imshow("Demo", frame)
+
+        if cv2.waitKey(1) == 27:
+            break
+
+
+    webcam.release()
+    cv2.destroyAllWindows()
+
+
+
+def Alti() : # 알티노 실행
+
+    count_time = []
+
+    open()
+
+    while True :
+        if str(left_pupil) and str(right_pupil) == "None" :
+            start_time = time.time()
+        else :
+            start_time = None
+
+        if time.time() - start_time >= 2 : # 2초 이상 none이 출력되는 경우
+            count_time += 1
+            sound(41) # 경고음 주는 코드 / 도37 레39 미41 파42 솔44 라46 시48
+
+        if len(count_time) >= 3 : # 경고음이 3번 이상인 경우 (비상 깜빡이 점등)
+            key_input = input("key : ")
+            while True :
+                led(3) # 전방 점등
+                delay(700) # 0.7초동안 지속
+                led(0) # 소등
+                if key_input == 'q' or 'Q' :
+                    break
+
+    close()
+
+
+
+
+
+
+
+
+cam()
+Alti()
